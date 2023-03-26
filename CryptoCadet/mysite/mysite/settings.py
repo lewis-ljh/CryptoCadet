b6a0023ce272a99910d1b8c725209d04c8240dee
Tajul <ec21385@qmul.ac.uk>
2023-03-26 11:33:15 +0100

    Login and logout

--- CryptoCadet/mysite/mysite/settings.py
+++ CryptoCadet/mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'register.apps.RegisterConfig'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
