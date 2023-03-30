b46f92d9838dab03e4121875db9299335305262c
Cameron <cpjules@gmail.com>
2023-03-30 19:39:08 +0100

    Price update in database

--- CryptoCadet/mysite/main/views.py
+++ CryptoCadet/mysite/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .APIManager import *
-
+from .models import Coin
 # Create your views here.
 
 def home(response):
@@ -17,3 +17,17 @@ def viewCrypto(response):
 
 def logout(response):
     return render(response, "main/log-out.html")
+
+#Load the page for viewing coins -> when loading loop though all coins in DB and update price from API
+def cryptoList(response):
+    coins = Coin.objects.all()
+    for coin in coins:
+        dict = getTickers()
+        #find each coin in the api return
+        found =  next(item for item in dict if item["symbol"] == coin.ticker)
+        price = found['price']
+        #set price and save it 
+        coin.price = price
+        coin.save()
+        #render crytpo list
+    return render(response, "main/cryptoList.html" ,{"price":price})
