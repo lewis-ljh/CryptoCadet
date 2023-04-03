7228d8813266aa1e33910251415d8a17e3d1a344
Cameron <cpjules@gmail.com>
2023-04-03 18:44:50 +0100

    revert

--- CryptoCadet/mysite/watchlist/views.py
+++ CryptoCadet/mysite/watchlist/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from .models import WatchCoin
 # Create your views here.
 def watchList(response):
-    try:
-        items = WatchCoin.objects.all().filter(user=response.user)
-    except WatchCoin.DoesNotExist:
-        items = None
+    items = WatchCoin.objects.all()
     return render(response, "watchlist/watchlist.html" ,{"items":items})
