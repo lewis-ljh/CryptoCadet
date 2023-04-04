5b7c63dccda4db04157375078f293ac982abbf02
farezae <85083961+farezae@users.noreply.github.com>
2023-04-04 13:37:04 +0100

    Update urls.py
    added a path for exam

--- CryptoCadet/mysite/mysite/urls.py
+++ CryptoCadet/mysite/mysite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("register/", v.register, name="register"),
     path("", include("main.urls")),
     path("", include("django.contrib.auth.urls")),
+    path('exam/', include('exam.urls'))
     
 ]
