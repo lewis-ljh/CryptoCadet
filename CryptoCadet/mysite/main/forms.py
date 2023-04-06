e1e63057ba1ced9f07b8e730ada8d3a18a1ed0ce
lewis_ljh <lewishoggarth@icloud.com>
2023-04-06 10:32:55 +0100

    boostrap for tickets page

--- CryptoCadet/mysite/main/forms.py
+++ CryptoCadet/mysite/main/forms.py
@@ -1,16 +1,7 @@
 from django import forms
-from .models import Ticket
+
 
 class CreateNewList(forms.Form):
     name = forms.CharField(label="Name", max_length=200)
     check = forms.BooleanField(required=False)
-
-class TicketForm(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ['title', 'query']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'query': forms.Textarea(attrs={'class': 'form-control'}), 
-        }
 
